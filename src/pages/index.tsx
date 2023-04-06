a64e2ff27fe483dc1366a6eec973d8a3a52938b4
TeUm3048 <teum3048@yandex.ru>
2023-04-06 18:47:56 +0300

    Fix redirect homepage to login, if not login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,21 +2,28 @@ import Layout from '@/components/Layout/Layout';
 import { SingupLayout } from '@/components/SingupLayout/SingupLayout';
 import css from './Home.module.css';
 import useToken from '@/utils/Token/token';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 
 
 function HomePage() {
 
     const [token, setToken] = useToken();
-    if (!token) {
-        return <SingupLayout setToken={setToken} />;
-    }
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!token) {                   // TODO write normal session check. This is very very ease to bypass.
+            router.push('/login');
+        }
+    }, [])
+
     return (
         <Layout>
             <div className={css.wrapper}>
                 <header className={css.header}>
                     <h1 className={css.logo}>LetiCoin</h1>
-                    <p className={css.subtext}>Comming Soon...</p>
+                    <p className={css.subtext}>Coming Soon...</p>
                 </header>
             </div>
         </Layout>
