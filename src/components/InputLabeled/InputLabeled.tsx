8061170ed0cf8ee11d56d9abafdafdb7100ed0d6
TeUm3048 <teum3048@yandex.ru>
2023-04-19 02:11:12 +0300

    Fix Firefox input autocomplete buy: Add ref to input component

--- src/components/InputLabeled/InputLabeled.tsx
+++ src/components/InputLabeled/InputLabeled.tsx
@@ -1,18 +1,21 @@
 import css from "./InputLabeled.module.css"
+import { ForwardedRef, forwardRef } from "react"
 
 
 type Props = JSX.IntrinsicElements['input'] & {
     label?: string,
 }
 
-
-const InputLabeled = ({ label, type, placeholder, value, ...other }: Props) => {
+const InputLabeled = forwardRef(({ label, type, placeholder, value, ...other }: Props, ref: ForwardedRef<HTMLInputElement | null>) => {
     return (
         <label>
             <div className={css.label}>{label}</div>
-            <input className={css.input} type={type || "text"} placeholder={placeholder} {...other} />
+            <input className={css.input} type={type || "text"} placeholder={placeholder} ref={ref} {...other} />
         </label>
     )
-}
+});
+
+InputLabeled.displayName = "InputLabeled";
+
 
 export default InputLabeled
